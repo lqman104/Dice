dd37fd1341af5c3be7d89b94fb374b1b7327f7ab
Luqman <lqman104@gmail.com>
2023-07-18 20:00:10 +0700

    feat: add state on the dice and separate to another func

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,7 +8,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dice'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +17,41 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDiceState = 1;
+  int rightDiceState = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          dice("images/dice$leftDiceState.png"),
+          dice("images/dice$rightDiceState.png")
+        ],
+      ),
+    );
+  }
+
+  Widget dice(String image) {
+    return Expanded(
+      child: TextButton(
+        child: Image.asset(image),
+        onPressed: () {
+          setState(() {
+            leftDiceState = Random().nextInt(6) + 1;
+            rightDiceState = Random().nextInt(6) + 1;
+          });
+        },
+      ),
+    );
   }
 }
